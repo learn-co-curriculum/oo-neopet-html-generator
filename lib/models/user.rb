cfe183fb1f5f3357ee6730e91df6c2de2bb73816
Katie Hoffman <ktahoffman@gmail.com>
2014-10-03 15:39:09 -0400

    passing 11 tests

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -1,12 +1,33 @@
+require 'pry'
+
 class User
   attr_reader :name
   attr_accessor :neopets, :neopoints, :items
+
+  PET_NAMES = ["Angel", "Baby", "Bailey", "Bandit", "Bella", "Buddy", "Charlie", "Chloe", "Coco", "Daisy", "Lily", "Lucy", "Maggie", "Max", "Molly", "Oliver", "Rocky", "Shadow", "Sophie", "Sunny", "Tiger"]
 
   def initialize(name)
     @name = name
     @neopets = []
     @items = []
     @neopoints = 2500
+  end
+
+  def select_pet_name
+    current_pets = self.neopets.collect {|pet| pet.name }
+    name = PET_NAMES.sample
+    while current_pets.include?(name)
+      name = PET_NAMES.sample
+    end
+    name
+  end
+
+  def buy_neopet
+    self.neopets << Neopet.new(select_pet_name)
+  end
+
+  def write_index_page
+    html = "!DOCTYPE"
   end
 
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -53,4 +53,23 @@ describe "User" do
     end
   end
 
+  describe "#select_pet_name" do
+    it "uses the PET_NAMES array to choose a unique pet name" do
+      modified_names = ["Angel", "Baby", "Bailey", "Bella", "Buddy", "Charlie", "Chloe", "Coco", "Lily", "Lucy", "Maggie", "Max", "Molly", "Oliver", "Shadow", "Sophie", "Sunny", "Tiger"]
+      modified_names.each {|n| mandy.neopets << Neopet.new(n)}
+      expect(mandy.select_pet_name).to satisfy {|s| ["Daisy","Rocky","Bandit"].include?(s) }
+    end
+  end
+
+  describe "#buy_neopet" do
+    it "creates a new instance of Neopet and adds pet to user's neopets" do
+      initial_pet_number = mandy.neopets.length
+      mandy.buy_neopet
+      expect(mandy.neopets.length).to eq(initial_pet_number + 1)
+      mandy.neopets.each do |pet|
+        expect(pet.class).to eq(Neopet)
+      end
+    end
+  end
+
 end
