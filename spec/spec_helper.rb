2c063f7c01936e023366ce8f526b3c35d276d719
Katie Hoffman <ktahoffman@gmail.com>
2014-10-04 20:27:47 -0400

    disappear warning w/correct capitalization

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,13 +2,13 @@ require_relative "../lib/models/user.rb"
 require_relative "../lib/models/neopet.rb"
 require_relative "../lib/models/item.rb"
 
-require 'FileUtils'
+require 'fileutils'
 
 RSpec.configure do |config|
   config.order = 'default'
-  # config.before(:all) do
-  #   FileUtils.rm_rf(Dir["views/users/*"]) # remove pre-existing view files
-  # end
+  config.before(:all) do
+    FileUtils.rm_rf(Dir["views/users/*"]) # remove pre-existing view files
+  end
   config.after(:all) do
     FileUtils.rm_rf(Dir["views/users/*"]) # remove created view files
   end
